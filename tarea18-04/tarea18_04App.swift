a5ed98475f884916017b3d484904f89df83065a9
jean <jeanneyra06@gmail.com>
2024-04-17 11:09:32 -0700

    Initial Commit

---
+++ tarea18-04/tarea18_04App.swift
@@ -0,0 +1,17 @@
+//
+//  tarea18_04App.swift
+//  tarea18-04
+//
+//  Created by Jeanpierre on 17/4/24.
+//
+
+import SwiftUI
+
+@main
+struct tarea18_04App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ tarea18-04UITests/tarea18_04UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  tarea18_04UITestsLaunchTests.swift
+//  tarea18-04UITests
+//
+//  Created by Jeanpierre on 17/4/24.
+//
+
+import XCTest
+
+class tarea18_04UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
